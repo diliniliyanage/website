e0b8805ac2500f94bae8c65a8a90f2a5c774a47c
dilini <diliniliyanage93@gmail.com>
2018-09-25 11:41:23 +0530

    first commit

---
+++ src/Providers/WebsiteServiceProviders.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace eyepax\website\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class WebsiteServiceProviders extends ServiceProvider{
+    public function boot(){
+        //dd('Loading service provider packages');
+    }
+    public function register(){
+
+    }
+}
+
